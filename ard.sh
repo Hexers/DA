9b16d2a5224a471b9d5112da6fba37d885c40e9b
Aleksandar Kljaic <Hexers@users.noreply.github.com>
2021-06-09 14:13:34 -0500

    Create ard.sh

---
+++ ard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script:
+# - enables the Remote Management service (ARD)
+# - grants access and all privileges to the user "admin"
+# - restarts the ARD agent and the ARD menu extra
+#
+# This script must be run as root
+#
+
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users admin -privs -all -restart -agent -menu
