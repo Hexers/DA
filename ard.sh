877386b6b8090f6ba877b083de8e09ed5094928a
Aleksandar Kljaic <Hexers@users.noreply.github.com>
2021-06-16 15:45:24 -0500

    Update ard.sh

--- ard.sh
+++ ard.sh
@@ -8,4 +8,4 @@
 # This script must be run as root
 #
 
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users admin -privs -all -restart -agent -menu
+sudo "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart" -activate -configure -access -on -users admin -privs -all -restart -agent -menu
